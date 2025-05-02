27f9fc3d4af4b92b20f861a9e92a79f955b966ee
Lucia <luciaandreeaursu@yahoo.com>
2025-05-02 10:20:25 +0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ShipsComponent } from './ships.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "ships", component: ShipsComponent},
+    {path : '', redirectTo:'/ships', pathMatch: 'full'}
+];
