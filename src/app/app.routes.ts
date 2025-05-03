3410a805ce1bd058e001146a77f005fb02802f35
Lucia <luciaandreeaursu@yahoo.com>
2025-05-03 10:20:43 +0300

    add the routes for the other components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { ShipsComponent } from './ships.component';
+import { VoyagesComponent } from './voyages.component';
+import { PortsComponent } from './ports.component';
+import { CountriesvisitedComponent } from './countriesvisited.component';
 
 export const routes: Routes = [
     {path : "ships", component: ShipsComponent},
-    {path : '', redirectTo:'/ships', pathMatch: 'full'}
+    {path : 'voyages', component : VoyagesComponent},
+    {path : "countriesvisited", component: CountriesvisitedComponent},
+    {path : "ports" , component: PortsComponent},
+    {path :'', redirectTo: '/ships', pathMatch: "full"}
+    
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule{}
