6bf2e00b35567a92bf6cbc8a5200c8e69d8ee2a5
Lucia <luciaandreeaursu@yahoo.com>
2025-05-03 13:00:45 +0300

    athhe the new route for voyage-chart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { ShipsComponent } from './ships.component';
 import { VoyagesComponent } from './voyages.component';
 import { PortsComponent } from './ports.component';
 import { CountriesvisitedComponent } from './countriesvisited.component';
+import { VoyagesChartComponent } from './voyages-chart/voyages-chart.component';
 
 export const routes: Routes = [
     {path : "ships", component: ShipsComponent},
     {path : 'voyages', component : VoyagesComponent},
     {path : "countriesvisited", component: CountriesvisitedComponent},
     {path : "ports" , component: PortsComponent},
+    {path : 'voyages-chart', component : VoyagesChartComponent},
     {path :'', redirectTo: '/ships', pathMatch: "full"}
     
 ];
